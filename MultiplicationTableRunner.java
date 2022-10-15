88a3eba4d55634783f13543c175cebcc8af36544
Chris <72232622+chrisdk95@users.noreply.github.com>
2022-10-15 00:12:58 -0700

    Create MultiplicationTableRunner.java

---
+++ MultiplicationTableRunner.java
@@ -0,0 +1,13 @@
+package com.in28minutes.firstjavaproject;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MultiplicationTable table = new MultiplicationTable();
+		//table.print();
+		table.print(6, 11, 20);
+
+	}
+
+}
