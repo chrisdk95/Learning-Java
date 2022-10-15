2df40013a9797a3830820fec293a9f9b047b1d43
Chris <72232622+chrisdk95@users.noreply.github.com>
2022-10-15 00:11:59 -0700

    Create MultiolicationTable.java

---
+++ MultiolicationTable.java
@@ -0,0 +1,24 @@
+package com.in28minutes.firstjavaproject;
+
+public class MultiplicationTable {
+	// 5*1 = 5
+	//5*10 = 50
+	void print() {
+		for(int i = 1; i<= 12; i++) {
+			System.out.printf("%d * %d = %d", 5, i, 5*i).println();;
+		}
+	}
+
+	void print(int table) {
+		for(int i = 1; i<= 12; i++) {
+			System.out.printf("%d * %d = %d", table, i, table*i).println();;
+		}
+}
+
+	void print(int table, int from, int to) {
+		for (int i = from; i <= to; i++) {
+			System.out.printf("%d * %d = %d", table, i, table * i).println();
+			;
+		}
+}
+}
